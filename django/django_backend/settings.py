b0e584aca0ff6ba34674a77f2baaa849ec33db30
weosk <satzger.benjamin@gmail.com>
2020-11-28 20:17:28 +0100

    settings wanted to come along

--- django/django_backend/settings.py
+++ django/django_backend/settings.py
@@ -10,10 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-from pathlib import Path
-import os
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -147,6 +143,7 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
+
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
